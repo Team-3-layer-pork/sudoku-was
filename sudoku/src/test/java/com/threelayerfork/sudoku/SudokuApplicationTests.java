7cdb771369e792070753c49ac9fc64efde5ca551
greensky0526 <dbswovlf2009@naver.com>
2022-12-11 16:02:25 +0900

    project init

---
+++ sudoku/src/test/java/com/threelayerfork/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threelayerfork.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
