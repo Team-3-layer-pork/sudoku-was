bc869f1e30a5fe7cbbeebf0dbcd7f0a59e12df75
20jun <ex_xe@naver.com>
2022-12-16 01:47:38 +0900

    대강 레이아웃 잡기

--- sudoku/src/main/frontend/src/App.js
+++ sudoku/src/main/frontend/src/App.js
@@ -1,21 +1,46 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Reset } from "styled-reset";
+
+import Container from "react-bootstrap/Container";
+import { Button } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-  const [hello, setHello] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("/api/hello")
-      .then((response) => setHello(response.data))
-      .catch((error) => console.log(error));
-  }, []);
-
   return (
-    <div>
-      백엔드에서 가져온 데이터입니다 : {hello}
-      Test
-    </div>
+    <>
+      {/* CSS Style 초기화 */}
+      {/* <Reset /> */}
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Container
+          style={{ backgroundColor: "pink" }}
+          className="position-absolute top-50 start-50 translate-middle"
+        >
+          <Row>
+            <Col style={{ backgroundColor: "gray" }}>1 of 2</Col>
+            <Col>2 of 2</Col>
+          </Row>
+          {/* <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+          </Row> */}
+        </Container>
+      </div>
+    </>
   );
 }
 
